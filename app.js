728cf3d45f078e2cd4e7b0a3ef46162eb266e15b
Deepali06 <deepali.nayyar@wipro.com>
2019-04-08 13:51:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+//fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
+//fs.writeFileSync('notes.txt', 'My name is Deepali');
+fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');
