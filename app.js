df012c8022f084b401a43a2fa59bb1f4f3ab4a8a
Deepali06 <deepali.nayyar@wipro.com>
2019-04-08 15:08:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+/* 1st Program
 const fs = require('fs');
-//fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
-//fs.writeFileSync('notes.txt', 'My name is Deepali');
-fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');
+fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
+fs.writeFileSync('notes.txt', 'My name is Deepali');
+fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');*/
+
+/* 2nd Program */
+
+require('./utils.js');
+const name = 'Deepali';
+
+console.log('name');
