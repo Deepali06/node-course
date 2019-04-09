a6f8cb1c87834eba76bfad7119a3924fd4105ae4
Deepali06 <deepali.nayyar@wipro.com>
2019-04-09 15:09:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-/* 1st Program
-const fs = require('fs');
-fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
-fs.writeFileSync('notes.txt', 'My name is Deepali');
-fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');*/
+/* 1st Program */
+const validator = require('validator');
 
+//const fs = require('fs');
+//fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
+//fs.appendFileSync('notes.txt', 'My name is Deepali');
+//fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');
+console.log(validator.isEmail('example.com'));
 /* 2nd Program */
 
 //const add = require('./utils.js');
-const note = require('./notes.js');
+/*const note = require('./notes.js');
 const msg = note();
-console.log(msg);
+console.log(msg);*/
 //const name = 'Deepali';
 //const sum =  add(4,-2);
 //console.log(sum);
