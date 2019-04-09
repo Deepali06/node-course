b921647b1d1d764e4a2e5cb6ebe28d8ad964bded
Deepali06 <deepali.nayyar@wipro.com>
2019-04-09 17:27:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 /* 1st Program */
 const validator = require('validator');
-
+const chalk = require('chalk');
 //const fs = require('fs');
 //fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
 //fs.appendFileSync('notes.txt', 'My name is Deepali');
 //fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');
-console.log(validator.isEmail('example.com'));
+console.log(validator.isURL('https://mead.io'));
+console.log(chalk.green.bold.inverse("Success!"))
 /* 2nd Program */
 
 //const add = require('./utils.js');
