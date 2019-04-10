781519991c98f8c64a70df52eb70fd36a4705476
Deepali06 <deepali.nayyar@wipro.com>
2019-04-10 15:59:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,21 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note');
+    builder:{
+    title: {
+        describe: 'Note Title',
+        demandOption: true,
+        type: 'string'
+
+    },
+    body: {
+        describe: 'Note Body',
+        demandOption: true,
+        type: 'string'
+    }
+},
+    handler: function(argv){
+        console.log('Title: ' + argv.title + ' Body: ' + argv.body);
     }
 })
 
@@ -48,4 +61,5 @@ yargs.command({
         console.log('Reading a note');
     }
 })
-console.log(yargs.argv);
+yargs.parse();
+//console.log(yargs.argv);
