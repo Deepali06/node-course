c4125de75bf022011356f7353b6563c99be5a40b
Deepali06 <deepali.nayyar@wipro.com>
2019-04-10 19:09:53 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
 //console.log('notes.js');
-
+const fs = require('fs');
 
 const getnotes = function(){
     return 'Yournotes..';
 }
-module.exports = getnotes;
+
+const addnote = function(title,body){
+   const notes = loadnotes();
+   console.log(notes);
+}
+
+const loadnotes = function(){
+    try{
+        const dataBuffer = fs.readFileSync(notes.json);
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+
+    } catch(e){
+        return []
+    }
+    
+}
+module.exports = {
+    getnotes: getnotes,
+    addnote: addnote
+};
