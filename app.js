1dafd8012124ad308897cef49f8f3acb93ef020c
Deepali06 <deepali.nayyar@wipro.com>
2019-04-10 12:08:29 +0530

    Update app.js
    Using yargs for command line arguments

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 /* 1st Program */
 const validator = require('validator');
 const chalk = require('chalk');
-const command = process.argv[2];
+const yargs = require('yargs');
+//console.log(process.argv);
+// Customize yargs version
+yargs.version('1.1.0');
+console.log(yargs.argv);
 
-if(command === 'add') {
-    console.log('Adding a note!');
-}
-//const fs = require('fs');
-//fs.writeFileSync('notes.txt', 'This file was created by NodeJS');
-//fs.appendFileSync('notes.txt', 'My name is Deepali');
-//fs.appendFileSync('notes.txt',' I work at Wipro Digital as Senior Software Developer');
-/*console.log(validator.isURL('https://mead.io'));
-console.log(chalk.red.bold.inverse("Success!"))
 
-console.log(process.argv[2]);
-/* 2nd Program */
+// Create add command
 
-//const add = require('./utils.js');
-/*const note = require('./notes.js');
-const msg = note();
-console.log(msg);*/
-//const name = 'Deepali';
-//const sum =  add(4,-2);
-//console.log(sum);
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note'
+})
+// add, remove, read, list 
