6439dc8a248af77cdf0eb163501d9873d6f4b0a5
Deepali06 <deepali.nayyar@wipro.com>
2019-04-10 14:11:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,47 @@ const yargs = require('yargs');
 //console.log(process.argv);
 // Customize yargs version
 yargs.version('1.1.0');
-console.log(yargs.argv);
+
+// add, remove, read, list 
 
 
 // Create add command
 
 yargs.command({
     command: 'add',
-    describe: 'Add a new note'
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note');
+    }
 })
-// add, remove, read, list 
+
+// Create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note');
+    }
+})
+
+// Create List command
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function(){
+        console.log('Listing a note');
+    }
+})
+
+// Create a Read Command
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function(){
+        console.log('Reading a note');
+    }
+})
+console.log(yargs.argv);
