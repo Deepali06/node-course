be0157590cd9689f4ddd87e63e882ba4eb2a352d
Deepali06 <deepali.nayyar@wipro.com>
2019-04-10 19:23:17 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -7,12 +7,28 @@ const getnotes = function(){
 
 const addnote = function(title,body){
    const notes = loadnotes();
-   console.log(notes);
+   const duplicatenotes = notes.filter(function(note){
+      return note.title === title
+   })
+   if(duplicatenotes.length === 0){
+    notes.push({
+        title: title,
+        body: body
+    })
+    savenotes(notes);
+    console.log('New note added');
+   } else{
+       console.log('Note title taken');
+   }
+  
 }
-
+const savenotes = function(notes){
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJson);
+}
 const loadnotes = function(){
     try{
-        const dataBuffer = fs.readFileSync(notes.json);
+        const dataBuffer = fs.readFileSync('notes.json');
         const dataJson = dataBuffer.toString();
         return JSON.parse(dataJson);
 
